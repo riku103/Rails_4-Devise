8ea8f4f45eadd611bd743e3480159c284dfcd26b
新田陸人 <>
2020-12-31 17:20:17 +0900

    Add: 商品一覧ページへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/new'
-  get 'items/create'
+  root to: 'items#index'
   devise_for :users
+  resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
