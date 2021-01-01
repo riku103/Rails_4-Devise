06a1035d86d63ca0cd8eb5cea65a8f5ad829931c
新田陸人 <>
2021-01-01 17:00:38 +0900

    Add: usersのshowメソッドのみにルーティングを指定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
   resources :items
+  resources :users, only: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
